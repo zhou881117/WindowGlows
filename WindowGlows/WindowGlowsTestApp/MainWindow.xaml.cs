dd3673a8f8cc6327c4e4ab2d26356e98961ee2f3
Expression2 <xprssn2@users.noreply.github.com>
2014-09-20 09:23:30 +0000

    Initial check-in

---
+++ WindowGlows/WindowGlowsTestApp/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using WindowGlows;
+
+namespace WindowGlowsTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
